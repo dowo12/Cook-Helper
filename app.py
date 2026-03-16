a6d1acbf897356dfbb7fe7d607188d1ddf9b0263
dowo12 <hmds8973@gmail.com>
2026-03-16 11:45:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from google import genai
 client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash",
+    model="gemini-1.5-flash",
     contents="안녕?"
 )
 
